f7d1f5fe09acf07a1dbdd82a85e60fc0c75384b1
mathewacarter29 <mathewacarter29@vt.edu>
2022-05-23 23:55:47 -0400

    Created quick home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,14 @@
+import rye2021 from "../media/Rye2021.png";
+import "./HomePage.css";
+
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="main">
+      <h1>Welcome to Bayberry Volleyball!</h1>
+      <img src={rye2021} alt={rye2021}></img>
+      <h2>Insert more media below</h2>
+    </div>
+  );
 }
 
 export default HomePage;
